54dd855e1d8073be8e600c600050f98a65e23d2c
juhua.zjh <zjh407015@alibaba-inc.com>
2023-11-09 15:25:30 +0800

    updates

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -13,24 +13,24 @@ vector<vector<string>> read_file(const string& fileName) {
     input.open(fileName);
     vector<vector<string>> contents;
 
-    if ( input.is_open() ) {
+    if (input.is_open()) {
         string aline;
 
         // read line by line
-        while ( getline(input, aline) ) {
+        while (getline(input, aline)) {
             vector<string> content;
             string word;
             size_t i = 0;
 
             // remove the space in the front
-            while ( i < aline.size() && (aline[i] == ' ' || aline[i] == '\t') ) {
+            while (i < aline.size() && (aline[i] == ' ' || aline[i] == '\t')) {
                 i++;
             }
 
-            for ( ; i < aline.size(); i++ ) {
-                if ( aline[i] == ' ' || aline[i] == '\t' ) {
-                    while ( i < aline.size() &&
-                        (aline[i] == ' ' || aline[i] == '\t') ) {
+            for (; i < aline.size(); i++) {
+                if (aline[i] == ' ' || aline[i] == '\t') {
+                    while (i < aline.size() &&
+                        (aline[i] == ' ' || aline[i] == '\t')) {
                         i++;
                     }
                     i--;
@@ -43,12 +43,12 @@ vector<vector<string>> read_file(const string& fileName) {
             }
 
             // last word
-            if ( !word.empty() ) {
+            if (!word.empty()) {
                 content.push_back(word);
             }
 
             // skip the empty line
-            if ( !content.empty() ) {
+            if (!content.empty()) {
                 contents.push_back(content);
             }
         }
@@ -66,51 +66,51 @@ void parse_content(vector<vector<string>>& contents, Image& image, Raytracer& tr
     vector<Vec3> vertices; // store vertices to form a triangle
     Vec4 cur_color(1.0f, 1.0f, 1.0f, 1.0f); // default color
 
-    for ( auto& content : contents ) {
-        if ( content.size() >= 4 && content[0] == "png" ) {
+    for (auto& content : contents) {
+        if (content[0] == "png" && content.size() >= 4) {
             image.w = stoi(content[1]);
             image.h = stoi(content[2]);
             image.image_name = content[3];
         }
-        else if ( content.size() >= 5 && content[0] == "sphere" ) {
+        else if (content[0] == "sphere" && content.size() >= 5) {
             auto* new_sphere = new Sphere(Vec3(stof(content[1]), stof(content[2]), stof(content[3])), cur_color, stof(content[4]));
 
-            if ( image.is_shiny ) {
+            if (image.is_shiny) {
                 new_sphere->is_shiny = true;
                 new_sphere->shininess = image.shininess;
             }
-            if ( image.is_transparent ) {
+            if (image.is_transparent) {
                 new_sphere->is_transparent = true;
                 new_sphere->transparency = image.transparency;
             }
 
             objects.emplace_back(new_sphere);
         }
-        else if ( content.size() >= 4 && content[0] == "sun" ) {
+        else if (content[0] == "sun" && content.size() >= 4) {
             Sun* new_sun = new Sun(Vec3(stof(content[1]), stof(content[2]), stof(content[3])), cur_color);
             lights.emplace_back(new_sun);
         }
-        else if ( content.size() >= 4 && content[0] == "color" ) {
+        else if (content[0] == "color" && content.size() >= 4) {
             Vec4 temp_color = { stof(content[1]), stof(content[2]), stof(content[3]), 1.0f };
             cur_color = temp_color;
         }
-        else if ( content.size() >= 4 && content[0] == "bulb" ) {
+        else if (content[0] == "bulb" && content.size() >= 4) {
             Bulb* new_bulb = new Bulb(Vec3(stof(content[1]), stof(content[2]), stof(content[3])), cur_color);
             lights.emplace_back(new_bulb);
         }
-        else if ( content.size() >= 2 && content[0] == "expose" ) {
+        else if (content[0] == "expose" && content.size() >= 2) {
             image.is_expose = true;
             image.expose = stof(content[1]);
         }
-        else if ( content.size() >= 5 && content[0] == "plane" ) {
+        else if (content[0] == "plane" && content.size() >= 5) {
             auto* new_plane = new Plane(Vec4(stof(content[1]), stof(content[2]), stof(content[3]), stof(content[4])), cur_color);
             objects.emplace_back(new_plane);
         }
-        else if ( content.size() >= 4 && content[0] == "eye" ) {
+        else if (content[0] == "eye" && content.size() >= 4) {
             Vec3 new_eye = { stof(content[1]), stof(content[2]),stof(content[3]) };
             tracer.eye = new_eye;
         }
-        else if ( content.size() >= 4 && content[0] == "forward" ) {
+        else if (content[0] == "forward" && content.size() >= 4) {
             Vec3 new_forward = { stof(content[1]), stof(content[2]),stof(content[3]) };
             tracer.forward = new_forward;
             // Change up and right vectors to be perpendicular to the new forward.
@@ -121,7 +121,7 @@ void parse_content(vector<vector<string>>& contents, Image& image, Raytracer& tr
             Vec3 new_right = cross(new_up, new_forward);
             tracer.right = -1 * normalize(new_right);
         }
-        else if ( content.size() >= 4 && content[0] == "up" ) {
+        else if (content[0] == "up" && content.size() >= 4) {
             Vec3 input_up = { stof(content[1]), stof(content[2]), stof(content[3]) };
             // Use the closest possible up that is perpendicular to the existing forward. 
             // Then change the right vector to be perpendicular to forward and up.
@@ -132,46 +132,46 @@ void parse_content(vector<vector<string>>& contents, Image& image, Raytracer& tr
             Vec3 new_right = -1 * cross(new_up, existing_forward);
             tracer.right = normalize(new_right);
         }
-        else if ( content.size() >= 2 && content[0] == "shininess" ) {
+        else if (content[0] == "shininess" && content.size() >= 2) {
             image.is_shiny = true;
             image.shininess = stof(content[1]);
         }
-        else if ( content.size() >= 4 && content[0] == "xyz" ) {
+        else if (content[0] == "xyz" && content.size() >= 4) {
             Vec3 new_vertex(stof(content[1]), stof(content[2]), stof(content[3]));
             vertices.emplace_back(new_vertex);
         }
-        else if ( content.size() >= 4 && content[0] == "trif" ) {
+        else if (content[0] == "trif" && content.size() >= 4) {
             vector<int> idx;
-            for ( size_t i = 1; i < content.size(); i++ ) {
+            for (size_t i = 1; i < content.size(); i++) {
                 int tmp = stoi(content[i]);
                 idx.emplace_back(tmp > 0 ? tmp - 1 : int(vertices.size()) + tmp);
             }
             auto* new_triangle = new Triangle(vertices[idx[0]], vertices[idx[1]], vertices[idx[2]], cur_color);
             objects.emplace_back(new_triangle);
         }
-        else if ( content.size() >= 2 && content[0] == "transparency" ) {
+        else if (content[0] == "transparency" && content.size() >= 2) {
             image.is_transparent = true;
             image.transparency = stof(content[1]);
         }
     }
 }
 
-void generate_image(Image& img) {
-    auto* im = ( unsigned char* )calloc(img.w * img.h * 4,
+void generate_image(Image& image) {
+    auto* img = ( unsigned char* )calloc(image.w * image.h * 4,
         sizeof(unsigned char));
 
-    for ( size_t i = 0; i < img.pixels.size(); i++ ) {
-        im[((img.pixels[i].y * img.w) + img.pixels[i].x) * 4 + 0] =
-            char(img.pixels[i].r * 255.0f);
-        im[((img.pixels[i].y * img.w) + img.pixels[i].x) * 4 + 1] =
-            char(img.pixels[i].g * 255.0f);
-        im[((img.pixels[i].y * img.w) + img.pixels[i].x) * 4 + 2] =
-            char(img.pixels[i].b * 255.0f);
-        im[((img.pixels[i].y * img.w) + img.pixels[i].x) * 4 + 3] =
-            char(img.pixels[i].a * 255.0f);
+    for (size_t i = 0; i < image.pixels.size(); i++) {
+        img[((image.pixels[i].y * image.w) + image.pixels[i].x) * 4 + 0] =
+            char(image.pixels[i].r * 255.0f);
+        img[((image.pixels[i].y * image.w) + image.pixels[i].x) * 4 + 1] =
+            char(image.pixels[i].g * 255.0f);
+        img[((image.pixels[i].y * image.w) + image.pixels[i].x) * 4 + 2] =
+            char(image.pixels[i].b * 255.0f);
+        img[((image.pixels[i].y * image.w) + image.pixels[i].x) * 4 + 3] =
+            char(image.pixels[i].a * 255.0f);
     }
-    lodepng_encode32_file(img.image_name.c_str(), im, img.w, img.h);
-    free(im);
+    lodepng_encode32_file(image.image_name.c_str(), img, image.w, image.h);
+    free(img);
 }
 
 bool check_occlusion(Vec3 p, Object* self_object, vector<Object*>& objects, Light* lght) {
@@ -184,7 +184,7 @@ bool check_occlusion(Vec3 p, Object* self_object, vector<Object*>& objects, Ligh
     return false;
 }
 
-Vec4 compute_color(Vec3 r0, Vec3 ray, float n_t, Object* object, vector<Light*>& lights, vector<Object*>& objects, Image& img, int depth) {
+Vec4 compute_color(Vec3 r0, Vec3 ray, float n_t, Object* object, vector<Light*>& lights, vector<Object*>& objects, Image& image, int depth) {
     Vec3 p = r0 + n_t * normalize(ray);
     vector<Vec3> colors;
 
@@ -202,8 +202,8 @@ Vec4 compute_color(Vec3 r0, Vec3 ray, float n_t, Object* object, vector<Light*>&
     Vec3 diffuse_color = Vec3(0, 0, 0);
     for (auto color : colors) {
         diffuse_color = diffuse_color + color;
-        if (img.is_expose)
-            diffuse_color = expose(diffuse_color, img.expose);
+        if (image.is_expose)
+            diffuse_color = expose(diffuse_color, image.expose);
         diffuse_color = clamp(diffuse_color);
     }
 
@@ -211,15 +211,14 @@ Vec4 compute_color(Vec3 r0, Vec3 ray, float n_t, Object* object, vector<Light*>&
     if (object->is_shiny) {
         Vec3 reflect_dir = normalize(ray) - 2.0f * dot(n, normalize(ray)) * n;
         reflect_dir = normalize(reflect_dir);
-        specular_color = object->shininess * trace_ray(p, reflect_dir, object, lights, objects, img, depth + 1);
+        specular_color = object->shininess * trace_ray(p, reflect_dir, object, lights, objects, image, depth + 1);
         specular_color = clamp(specular_color);
     }
     Vec4 final_color = { clamp(diffuse_color + specular_color), object->color.w };
     return final_color;
 }
 
-Vec3 trace_ray(Vec3 r0, Vec3 ray, Object* object, vector<Light*>& lights,
-    vector<Object*>& objects, Image& img, int depth) {
+Vec3 trace_ray(Vec3 r0, Vec3 ray, Object* object, vector<Light*>& lights, vector<Object*>& objects, Image& image, int depth) {
     if (depth > 4) {
         return { 0, 0, 0 };
     }
@@ -240,7 +239,7 @@ Vec3 trace_ray(Vec3 r0, Vec3 ray, Object* object, vector<Light*>& lights,
 
     if (nearest_object != nullptr) {
         return to_vec3(
-            compute_color(r0, ray, min_t, nearest_object, lights, objects, img, depth));
+            compute_color(r0, ray, min_t, nearest_object, lights, objects, image, depth));
     }
     else {
         return { 0, 0, 0 };
